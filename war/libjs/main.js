f8d69533b18a16a3b924e7eb072beb5debca3bdd
skgitter <joinsunil@gmail.com>
2015-01-16 12:30:16 +0530

    company logo modified
    remove old logo with new one.

--- war/libjs/main.js
+++ war/libjs/main.js
@@ -11,8 +11,8 @@ jQuery(function($) {'use strict',
 	// accordian
 	$('.accordion-toggle').on('click', function(){
 		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
+			$(this).find('>.panel-heading').removeClass('active');
+		});
 
 	 	$(this).closest('.panel-heading').toggleClass('active');
 	});
